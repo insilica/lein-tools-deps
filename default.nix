01b6ede096b7ff5646180a46970be8c9a79b61d6
John Shaffer <jshaffer2112@protonmail.com>
2021-12-15 11:23:54 -0600

    Add Nix and direnv files

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  rev = "46725ae611741dd6d9a43c7e79d5d98ca9ce4328";
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "11srp3zfac0ahb1mxzkw3czlpmxc1ls7y219ph1r4wx2ndany9s9";
+  };
+  pkgs = import nixpkgs {};
+  inherit (pkgs) fetchurl lib stdenv;
+in with pkgs;
+mkShell {
+  buildInputs = [
+    leiningen
+  ];
+}
